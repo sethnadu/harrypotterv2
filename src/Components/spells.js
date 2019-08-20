7e198e90340f94267b0f3d0dd2623da2f6873382
Seth Nadu <seth.nadu@gmail.com>
2019-08-19 21:28:04 -0400

    added image to spells page

--- src/Components/spells.js
+++ src/Components/spells.js
@@ -5,41 +5,45 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import { getSpells } from "../Actions";
 import SpellsCard from "./spellsCard.js";
-import SpellsDropDown from "./Organizationcomponents/spellsDropDown.js"
-import "../App.css"
+import SpellsDropDown from "./Organizationcomponents/spellsDropDown.js";
+import "../App.css";
+
+import WandImage from "../Assets/wand.jpg";
 
 const useStyles = makeStyles(() => ({
-    container: {
-      margin: "30px",
-    },
-    searchContainer: {
-      margin: "120px 20px 0 20px",
-      fontFamily: "harry potter"
-    },
-    form: {
-      display: "flex",
-      flexDirection: "column",
-    },
-    textInput: {
-      width: "200px",
-      margin: "auto",
-      textAlign: "center",
-      padding: "5px",
-      borderRadius: "10px",
-      border: "2px solid #8d0901",
-      backgroundColor: "#f8c20e",
-      color: "#8d0901",
-      fontWeight: "bold",
-      fontSize: "1.2rem",
-      fontFamily: "cursive",
-    },
-    title: {
-        fontSize: "2.4rem"
-      }
-  
-  }));
-  
-
+  container: {
+    margin: "30px",
+  },
+  searchContainer: {
+    margin: "120px 20px 0 20px",
+    fontFamily: "harry potter",
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  textInput: {
+    width: "200px",
+    margin: "auto",
+    textAlign: "center",
+    padding: "5px",
+    borderRadius: "10px",
+    border: "2px solid #8d0901",
+    backgroundColor: "#f8c20e",
+    color: "#8d0901",
+    fontWeight: "bold",
+    fontSize: "1.2rem",
+    fontFamily: "cursive",
+  },
+  title: {
+    fontSize: "2.4rem",
+  },
+  image: {
+    maxWidth: "700px",
+    width: "100%",
+    borderRadius: "10px",
+  },
+}));
 
 const Spells = props => {
   const classes = useStyles();
@@ -51,13 +55,14 @@ const Spells = props => {
   };
 
   const handleType = name => {
-      setSearchSpell(name)
-      props.getSpells()
-  }
+    setSearchSpell(name);
+    props.getSpells();
+  };
 
   return (
     <div className={classes.container}>
       <h2 className={classes.title}>Spells</h2>
+      <img className={classes.image} src = {WandImage} alt ="Ollivanders shop drawing" />
       <form className={classes.form}>
         <p>Search Spell by Name</p>
         <input
@@ -68,21 +73,18 @@ const Spells = props => {
         />
       </form>
       <p>(Case Sensitive)</p>
-      <SpellsDropDown handleType = {handleType} getSpells = {props.getSpells}/>
+      <SpellsDropDown handleType={handleType} getSpells={props.getSpells} />
       {props.isLoading ? (
         <Loader type="BallTriangle" color="#e22121" height={100} width={100} />
       ) : (
         ""
       )}
 
-
       <div className={classes.searchContainer}>
         {props.spells.map(spell => {
           return (
-            spell.spell.includes(`${searchSpell}`) && 
-            searchSpell !== "" && (
-              <SpellsCard key={spell._id} spell={spell} />
-            )
+            spell.spell.includes(`${searchSpell}`) &&
+            searchSpell !== "" && <SpellsCard key={spell._id} spell={spell} />
           );
         })}
       </div>
@@ -101,9 +103,7 @@ const Spells = props => {
         {props.spells.map(spell => {
           return (
             searchSpell === "Hex" &&
-            spell.type === "Hex" && (
-              <SpellsCard key={spell._id} spell={spell} />
-            )
+            spell.type === "Hex" && <SpellsCard key={spell._id} spell={spell} />
           );
         })}
       </div>
